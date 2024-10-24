6e9c70b94b740439a3395e9600fad5fb68d36a1c
Looyyd <filip.a.niedzielski@gmail.com>
2024-10-24 20:21:25 +0200

    start db start extract azure and don't start all at the same time

--- apps/data-collection/start_data_collection.sh
+++ apps/data-collection/start_data_collection.sh
@@ -9,12 +9,17 @@ scripts=("collectMatchIds.ts" "fetchPuuids.ts" "processMatches.ts" "updateLadder
 # Create logs directory if it doesn't exist
 mkdir -p logs
 
+# Launch extractToAzure
+yarn tsx "./src/scripts/extractToAzure.ts" > "./logs/log_extractToAzure.txt" 2>&1 &
+sleep 30 # to avoid spikes in db usage
+
 # Launch all scripts for all regions
 for script in "${scripts[@]}"; do
     for region in "${regions[@]}"; do
         yarn tsx "./src/scripts/$script" --region "$region" > "./logs/log_${script%.ts}_${region}.txt" 2>&1 &
         echo "Started $script for $region"
+        sleep 30 # to avoid spikes in db usage
     done
 done
 
-echo "All jobs started. Use 'jobs' to list them."
+echo "All jobs started."
