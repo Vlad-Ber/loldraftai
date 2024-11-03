36467345a97df6c742f35676214e7617b4c3e6f6
Looyyd <filip.a.niedzielski@gmail.com>
2024-11-03 13:13:39 +0100

    automation runbook added

---
+++ apps/data-collection/vm-restart-runbook.ps1
@@ -0,0 +1,39 @@
+Param(
+    [Parameter(Mandatory = $true)]
+    [string]$SubscriptionId,
+
+    [Parameter(Mandatory = $true)]
+    [string]$VmName,
+    
+    [Parameter(Mandatory = $true)]
+    [string]$ResourceGroupName
+)
+
+# Connect using managed identity
+try {
+    Disable-AzContextAutosave -Scope Process
+    Connect-AzAccount -Identity
+    Set-AzContext -Subscription $SubscriptionId
+    
+    Write-Output "Successfully connected using Managed Identity"
+} catch {
+    Write-Error "Failed to connect: $_"
+    throw
+}
+
+# Get VM status
+$vm = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $VmName -Status
+$powerState = ($vm.Statuses | Where-Object { $_.Code -match 'PowerState' }).DisplayStatus
+
+Write-Output "Current VM state: $powerState"
+
+# If VM is deallocated, try to start it
+if ($powerState -eq "VM deallocated") {
+    Write-Output "Attempting to start VM $VmName"
+    Start-AzVM -ResourceGroupName $ResourceGroupName -Name $VmName
+}
+
+# Get final state
+$vm = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $VmName -Status
+$finalState = ($vm.Statuses | Where-Object { $_.Code -match 'PowerState' }).DisplayStatus
+Write-Output "Final VM state: $finalState"
