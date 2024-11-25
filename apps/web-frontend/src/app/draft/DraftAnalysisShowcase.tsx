1b14409a9e28d747305a3fe284c02d96691a9df3
Looyyd <filip.a.niedzielski@gmail.com>
2024-11-25 22:40:59 +0100

    tooltip shadcn

--- apps/web-frontend/src/app/draft/DraftAnalysisShowcase.tsx
+++ apps/web-frontend/src/app/draft/DraftAnalysisShowcase.tsx
@@ -18,12 +18,8 @@ export const DraftAnalysisShowcase = ({
           Predicted Winrate
         </h2>
         <div className="mb-1 flex w-full justify-between">
-          <p className="text-secondary">
-            {`${Math.round(team1Winrate)}% Blue Side`}
-          </p>
-          <p className="text-secondary">
-            {`Red Side ${Math.round(team2Winrate)}%`}
-          </p>
+          <p>{`${Math.round(team1Winrate)}% Blue Side`}</p>
+          <p>{`Red Side ${Math.round(team2Winrate)}%`}</p>
         </div>
         <WinrateBar team1Winrate={team1Winrate} />
         <h2 className="mt-4 text-center text-lg font-bold">Team Advantages</h2>
