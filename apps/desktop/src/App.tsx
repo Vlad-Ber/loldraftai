a094706b2019baf5f7883ecaee3d75824f70a5aa
Looyyd <filip.a.niedzielski@gmail.com>
2024-12-10 21:39:50 +0100

    working react electron setup

---
+++ apps/desktop/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@draftking/ui/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button variant="outline">Click me please</Button>
+    </>
+  );
+}
+
+export default App;
