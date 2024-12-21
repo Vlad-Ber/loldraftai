ea845ef0b3355408cef8b9282626d22000f09c73
Looyyd <filip.a.niedzielski@gmail.com>
2024-12-21 18:07:08 +0100

    fix example-request

--- apps/machine-learning/scripts/match-prediction/example-request.py
+++ apps/machine-learning/scripts/match-prediction/example-request.py
@@ -5,8 +5,7 @@ input_data = {
     "averageTier": "GRANDMASTER",
     "averageDivision": "I",
     "champion_ids": [36, 5, 61, 147, 235, 163, 427, 910, 21, 111],
-    "gameVersionMajorPatch": 14,
-    "gameVersionMinorPatch": 18,
+    "numerical_elo": 14 * 50 + 24,
 }
 
 response = requests.post("http://localhost:8000/predict", json=input_data)
