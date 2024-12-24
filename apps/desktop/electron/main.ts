57bbe53de6ef71c0d07574afa1fa66196512be49
Looyyd <filip.a.niedzielski@gmail.com>
2024-12-24 12:13:50 +0100

    added icons and app/window title

--- apps/desktop/electron/main.ts
+++ apps/desktop/electron/main.ts
@@ -62,7 +62,8 @@ function setupAutoUpdater(win: BrowserWindow) {
 
 function createWindow() {
   win = new BrowserWindow({
-    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+    icon: path.join(process.env.VITE_PUBLIC, "logo512.png"),
+    title: "DraftKing",
     webPreferences: {
       preload: path.join(__dirname, "preload.mjs"),
       contextIsolation: true,
