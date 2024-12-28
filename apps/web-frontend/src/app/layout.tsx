13ef66fdc548037b70d61441b0c29675403ae832
Looyyd <filip.a.niedzielski@gmail.com>
2024-12-28 17:25:53 +0100

    footer always on bottom

--- apps/web-frontend/src/app/layout.tsx
+++ apps/web-frontend/src/app/layout.tsx
@@ -61,52 +61,52 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <body
-        className={`font-sans ${inter.variable} min-h-screen bg-background text-foreground`}
-      >
+      <body className={`font-sans ${inter.variable}`}>
         <ThemeProvider>
-          <nav className="sticky top-0 z-50 border-b border-border/40 bg-neutral-950 ">
-            <NavigationMenu className="mx-auto px-4 py-3">
-              <NavigationMenuList className="flex justify-center gap-10">
-                <NavigationMenuItem>
-                  <Link href="/" legacyBehavior passHref>
-                    <NavigationMenuLink
-                      className={navigationMenuTriggerStyle()}
-                    >
-                      Home
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <Link href="/draft" legacyBehavior passHref>
-                    <NavigationMenuLink
-                      className={navigationMenuTriggerStyle()}
-                    >
-                      Draft Analysis
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <Link href="/download" legacyBehavior passHref>
-                    <NavigationMenuLink
-                      className={navigationMenuTriggerStyle()}
-                    >
-                      Download
-                    </NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-              </NavigationMenuList>
-            </NavigationMenu>
-          </nav>
+          <div className="flex min-h-screen flex-col bg-background text-foreground">
+            <nav className="sticky top-0 z-50 border-b border-border/40 bg-neutral-950">
+              <NavigationMenu className="mx-auto px-4 py-3">
+                <NavigationMenuList className="flex justify-center gap-10">
+                  <NavigationMenuItem>
+                    <Link href="/" legacyBehavior passHref>
+                      <NavigationMenuLink
+                        className={navigationMenuTriggerStyle()}
+                      >
+                        Home
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem>
+                    <Link href="/draft" legacyBehavior passHref>
+                      <NavigationMenuLink
+                        className={navigationMenuTriggerStyle()}
+                      >
+                        Draft Analysis
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem>
+                    <Link href="/download" legacyBehavior passHref>
+                      <NavigationMenuLink
+                        className={navigationMenuTriggerStyle()}
+                      >
+                        Download
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                </NavigationMenuList>
+              </NavigationMenu>
+            </nav>
 
-          <main className="flex-1">{children}</main>
+            <main className="flex-1">{children}</main>
 
-          <footer className="border-t border-border/40  bg-neutral-950">
-            <div className="container p-4 text-center text-sm text-muted-foreground mx-auto">
-              Last model update: {lastModified} on patch {latestPatch}. Contact
-              looyyd on Discord for bug reports or feature requests.
-            </div>
-          </footer>
+            <footer className="border-t border-border/40 bg-neutral-950">
+              <div className="container p-4 text-center text-sm text-muted-foreground mx-auto">
+                Last model update: {lastModified} on patch {latestPatch}.
+                Contact looyyd on Discord for bug reports or feature requests.
+              </div>
+            </footer>
+          </div>
         </ThemeProvider>
       </body>
     </html>
