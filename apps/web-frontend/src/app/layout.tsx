970bc6aca2bb2f4b08c6fd9e2c29934d4d10a437
Looyyd <filip.a.niedzielski@gmail.com>
2024-12-30 23:35:30 +0100

    analytics added

--- apps/web-frontend/src/app/layout.tsx
+++ apps/web-frontend/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 import "./globals.css";
@@ -108,6 +110,8 @@ export default async function RootLayout({
             </footer>
           </div>
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
