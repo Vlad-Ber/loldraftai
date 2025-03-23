ef0b10525e8d672331307eb97f1314373cfebed7
Looyyd <filip.a.niedzielski@gmail.com>
2025-03-24 00:08:58 +0100

    reincrease embed sizes

--- apps/machine-learning/utils/match_prediction/config.py
+++ apps/machine-learning/utils/match_prediction/config.py
@@ -12,10 +12,10 @@ class TrainingConfig:
         self.hidden_dims = [1536, 768, 384, 192]  # Both wider and one layer deeper
         self.dropout = 0.25  # Doubled dropout after adding positional embeddings
         self.learning_rate = 5e-4  # Lowered after positional embeddings were added
-        self.champion_embed_dim = 128
-        self.queue_type_embed_dim = 4
-        self.patch_embed_dim = 8
-        self.elo_embed_dim = 8
+        self.champion_embed_dim = 256
+        self.queue_type_embed_dim = 256
+        self.patch_embed_dim = 256
+        self.elo_embed_dim = 256
 
         # weight decay didn't change much when training for a short time at 0.001, but for longer trianing runs, 0.01 might be better
         self.weight_decay = 0.001
