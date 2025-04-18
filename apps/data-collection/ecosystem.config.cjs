bf99e71fbb1072df8eb584f461d7179e59dd97a5
Looyyd <filip.a.niedzielski@gmail.com>
2025-04-18 12:25:42 +0200

    stagger 30 min between regions instead of 1hour

--- apps/data-collection/ecosystem.config.cjs
+++ apps/data-collection/ecosystem.config.cjs
@@ -16,11 +16,11 @@ module.exports = {
       ["collectMatchIds", "fetchPuuids", "processMatches", "updateLadder"].map(
         (script) => ({
           name: `${script}-${region}`,
-          // Delay each region's ladder updates by 1 hour to stagger the load on the database
+          // Delay each region's ladder updates by 30 minutes to stagger the load on the database
           script:
             script === "updateLadder"
               ? `sleep ${
-                  regionIndex * 3600
+                  regionIndex * 1800
                 } && yarn tsx ./src/scripts/${script}.ts --region ${region}`
               : `yarn tsx ./src/scripts/${script}.ts --region ${region}`,
           autorestart: true,
